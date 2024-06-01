5c2af898530d0ac56bbb1e5ba1bc34ce4c30aa0e
kareem <kareem@kareem.lan>
2024-06-01 16:53:01 +0100

    #GG4

---
+++ cmd/mygit/init.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func InitCommandHandler() {
+	for _, dir := range []string{".git", ".git/objects", ".git/refs"} {
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
+		}
+	}
+
+	headFileContents := []byte("ref: refs/heads/main\n")
+	if err := os.WriteFile(".git/HEAD", headFileContents, 0644); err != nil {
+		fmt.Fprintf(os.Stderr, "Error writing file: %s\n", err)
+	}
+
+	fmt.Println("Initialized git directory")
+}
